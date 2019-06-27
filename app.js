6794aa78347866c34d734da9c04989292855ad75
vnkvp <vnkvp@outlook.com>
2019-06-27 20:58:04 -0300

    debug

--- app.js
+++ app.js
@@ -1,70 +1,44 @@
+var createError = require('http-errors');
 var express = require('express');
-var bodyParser = require('body-parser');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var expressValidator = require('express-validator');
 var session = require('express-session');
+var logger = require('morgan');
+var expressValidator = require('express-validator');
 var mongo = require('mongodb');
 var db = require('monk')('localhost/blog');
 var multer = require('multer');
 var flash = require('connect-flash');
+var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
-//setup do view engine
-
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-//file uploads & multipart data
-
-app.use(multer({dest: './public/images/uploads'}));
-
-
-//tirar comment quando adicionar favicon em /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+/*file uploads & multipart data
+app.use(multer({dest:'./uploads/'})).single('photo');*/
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 //express session
-
 app.use(session({
 secret: 'secret',
 saveUninitialized: true,
 resave: true
 }));
 
-//express validator
-
-app.use(expressValidator({
-errorFormatter: (param, msg, value)=>{
-    var namespace = param.split('.')
-    , root = namespace.shift()
-    , formParam = root;
-
-    while(namespace.length){
-        formParam += '[' + namespace.shift() + ']';
-    }
-    return{
-        param: formParam,
-        msg: msg,
-        value: value
-    };
-}
-}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 //connect-flash
-
 app.use(flash());
 app.use((req, res, next)=>{
 res.locals.messages = require('express-messages')(req, res);
@@ -72,21 +46,28 @@ next();
 });
 
 //fazendo o database acessivel para o router
-
 app.use((req, res, next)=>{
-req.db = db;
-next();
+  req.db = db;
+  next();
+  });
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-app.use('/', routes);
-app.use('/users', users); 
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//catch 404 e ir direto para o gerenciador de erros
-
-app.use((req, res, next)=>{
-var err = new Error('Not found');
-err.status = 404;
-next(err);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-
+module.exports = app;
